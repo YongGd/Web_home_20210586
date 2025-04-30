53f0595a332755adec466be1bfb6a0617bd5ff36
admin <dlrhksdyd156@naver.com>
2025-04-30 10:49:23 +0900

    9주차 깃허브

--- js/js_popup.js
+++ js/js_popup.js
@@ -14,6 +14,34 @@
 //     window.close(); // 윈도우 닫기
 //     }               //윈도우 창을 다 닫아버려서서 js 파일을 하나 더 만들어야할듯. 인덱스에서도 js을 호출하기때문에
 
+function setCookie(name, value, expiredays) {
+    var date = new Date();
+    date.setDate(date.getDate() + expiredays);
+    document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "; path=/";SameSite=None; Secure;
+    }
+
+function getCookie(name) {
+    var cookie = document.cookie;
+    console.log("쿠키를 요청합니다.");
+        if (cookie != "") {
+             var cookie_array = cookie.split("; ");
+        for ( var index in cookie_array) {
+            var cookie_name = cookie_array[index].split("=");
+        if (cookie_name[0] == "popupYN") {
+        return cookie_name[1];
+        }
+        }
+        }
+        return ;
+        }
+function closePopup() {
+        if (document.getElementById('check_popup').value) {
+            setCookie("popupYN", "N", 1);
+            console.log("쿠키를 설정합니다.");
+            self.close();
+            }
+        }
+
 function show_clock(){
     let currentDate = new Date(); // 현재 시스템 날짜 객체 생성
     let divClock = document.getElementById('divClock');
@@ -38,6 +66,12 @@ function show_clock(){
 
     function pop_up() {
         window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+        var cookieCheck = getCookie("popupYN");
+        if (cookieCheck != "N"){
+            window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+        }
+
+
         }
     const over = (obj) => {
         obj.src = "image/LOGO.png";
@@ -46,6 +80,8 @@ function show_clock(){
         const c = '검색을 수행합니다';
         alert(c);
         };
+
+
                
     // function over(obj) {
     //     obj.src="image/Logo2.jpg";
